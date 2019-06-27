fd06aa92fd585a1a7506498c88ace860ca2714ba
yyj <yyj@jyy.com>
2019-06-27 18:11:31 +0800

    push code

---
+++ WindowsFormsApp/MainForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileListView = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
+            this.选择ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FileListView
+            // 
+            this.FileListView.Location = new System.Drawing.Point(12, 28);
+            this.FileListView.Name = "FileListView";
+            this.FileListView.Size = new System.Drawing.Size(686, 335);
+            this.FileListView.TabIndex = 0;
+            this.FileListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 49);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(710, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Location = new System.Drawing.Point(0, 25);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(710, 24);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // menuStrip3
+            // 
+            this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.选择ToolStripMenuItem});
+            this.menuStrip3.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip3.Name = "menuStrip3";
+            this.menuStrip3.Size = new System.Drawing.Size(710, 25);
+            this.menuStrip3.TabIndex = 3;
+            this.menuStrip3.Text = "menuStrip3";
+            // 
+            // 选择ToolStripMenuItem
+            // 
+            this.选择ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.选择ToolStripMenuItem.Name = "选择ToolStripMenuItem";
+            this.选择ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.选择ToolStripMenuItem.Text = "选择";
+            this.选择ToolStripMenuItem.Click += new System.EventHandler(this.SelectToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "文件夹";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem1_Click);
+            this.toolStripMenuItem1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ToolStripMenuItem1_MouseUp);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 375);
+            this.ControlBox = false;
+            this.Controls.Add(this.FileListView);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.menuStrip3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Ayyj";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip3.ResumeLayout(false);
+            this.menuStrip3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListView FileListView;
+        private MenuStrip menuStrip1;
+        private MenuStrip menuStrip2;
+        private MenuStrip menuStrip3;
+        private ToolStripMenuItem 选择ToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ WindowsFormsApp/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            for (var i = 0; i < 3; i++)
+            {
+                ColumnHeader ch = new ColumnHeader
+                {
+                    Text = "列标题" + i,
+                    Width = 120,
+                    TextAlign = HorizontalAlignment.Left
+                };
+                FileListView.Columns.Add(ch);
+                FileListView.BeginUpdate();
+            }
+            FileListView.View = View.Details;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SelectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("SelectToolStripMenuItem_Click");
+        }
+
+        private void ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("ToolStripMenuItem1_Click");
+        }
+
+        private void ToolStripMenuItem1_MouseUp(object sender, MouseEventArgs e)
+        {
+            FileListView.BeginUpdate();  //数据更新，UI暂时挂起，直到EndUpdate绘制控件，可以有效避免闪烁并大大提高加载速度 
+
+            var index = FileListView.Items.Count + 1;
+            ListViewItem lvi = new ListViewItem
+            {
+                ImageIndex = index,     //通过与imageList绑定，显示imageList中第i项图标 
+                Text = "subitem" + index
+            };
+            lvi.SubItems.Add("第2列,第" + index + "行");
+            lvi.SubItems.Add("第3列,第" + index + "行");
+            FileListView.Items.Add(lvi);
+            FileListView.EndUpdate();  //结束数据处理，UI界面一次性绘制。
+        }
+
+        //private void OpenFileBtn_Click(object sender, EventArgs e)
+        //{
+        //    OpenFileDialog dialog = new OpenFileDialog();
+        //    dialog.ShowDialog();
+        //    if (dialog.FileName != string.Empty)
+        //    {
+        //        ScriptEngine pyEngine = Python.CreateEngine();
+        //        dynamic py = pyEngine.ExecuteFile(@"charge2ascll.py");
+        //        string dd = py.main(dialog.FileName);//调用脚本文件中对应的函数
+
+        //        MessageBox.Show(dd + "\r\n");
+        //    }
+        //}
+
+        //private void PyTestBtn_Click(object sender, EventArgs e)
+        //{
+        //    ScriptEngine pyEngine = Python.CreateEngine();
+        //    dynamic py = pyEngine.ExecuteFile(@"test.py");
+        //    string dd = py.main("测试");//调用脚本文件中对应的函数
+
+        //    MessageBox.Show(dd + "\r\n");
+        //}
+    }
+}
